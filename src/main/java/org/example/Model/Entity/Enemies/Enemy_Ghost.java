65a31de09e071a5781e39f99df3556e857595b76
Fics3 <114524392+Fics3@users.noreply.github.com>
2023-07-14 14:03:44 +0300

    enemies update

--- src/main/java/org/example/Model/Entity/Enemies/Enemy_Ghost.java
+++ src/main/java/org/example/Model/Entity/Enemies/Enemy_Ghost.java
@@ -4,12 +4,12 @@ import org.example.Model.Entity.Entity;
 import org.example.Model.Main.GameCFG;
 import org.example.Model.PathFinder;
 
-public class Enemy_GreenBoy extends Entity {
-    public Enemy_GreenBoy(GameCFG gameCFG) {
+public class Enemy_Ghost extends Entity {
+    public Enemy_Ghost(GameCFG gameCFG) {
         super(gameCFG);
         setPathFinder(new PathFinder(gameCFG));
         setEnemy(true);
-        setMaxHP(50);
+        setMaxHP(30);
         setBaseHP(getMaxHP());
         setLvl(1);
         setHP(getMaxHP()*getLvl());
@@ -17,9 +17,8 @@ public class Enemy_GreenBoy extends Entity {
         setSpeed(2);
         setMaxMana(20);
         setMana(getMaxMana()*getLvl());
-        setName("GreenBoy");
+        setName("Ghost");
         setDamage(4*getLvl());
-        setDialogue("Sample Text");
     }
 
 }
